239d9108401dc6839bb3e25e6750189c88760958
Nicolas Favre <khronozz-dev@proton.me>
2023-06-25 00:53:38 +0200

    Add news website list to archive

--- news-archiver-scraper/supabase/functions/news-scraper/index.ts
+++ news-archiver-scraper/supabase/functions/news-scraper/index.ts
@@ -27,10 +27,14 @@
 import {serve} from "std/server";
 import puppeteer from 'puppeteer';
 import {createClient} from "@supabase/supabase-js";
+import * as data from './news_websites.json' assert {type: "json"};
 
 serve(async () => {
     try {
         console.log("[x] Scraping news...")
+
+        // Access the news_url array
+        const newsUrls = data.default.news_url;
 
         // Connect to Supabase
         const supabase = createClient(Deno.env.get("SUPABASE_URL"), Deno.env.get("SUPABASE_KEY"))
@@ -40,44 +44,46 @@ serve(async () => {
             browserWSEndpoint: `wss://chrome.browserless.io?token=${Deno.env.get(
                 "PUPPETEER_BROWSERLESS_IO_KEY"
             )}`,
-        });
-        const page = await browser.newPage();
-
-        await page.setViewport({
-            width: 1920,
-            height: 1080,
-            deviceScaleFactor: 3,
+            args: ['--start-maximized'],
         });
 
-        // Screenshot Wikipedia page
-        await page.goto("https://en.wikipedia.org/wiki/Rickrolling", {waitUntil: "networkidle2"});
-        const screenshot = await page.screenshot();
+        // Loop through news websites
+        for (const newsPage of newsUrls) {
 
-        // Set file type to png
-        const file = new File([screenshot], "rickroll.png", {type: "image/png"});
+            const page = await browser.newPage();
 
-        // Set filename with date
-        const date = Date.now()
-        const filename = "rickroll_" + date + ".png"
-
-        // Upload file to Supabase Storage
-        const {data, error} = await supabase.storage
-            .from("news-archives")
-            .upload("wikipedia/" + filename, file, {
-                cacheControl: "3600",
-                upsert: false,
+            await page.setViewport({
+                width: 1920,
+                height: 1080,
             });
 
-        console.log("Data returned from Supabase: ", data)
-        console.log("Error returned from Supabase: ", error)
+            // Screenshot Wikipedia page
+            await page.goto(newsPage.url, {waitUntil: "networkidle2"});
+            const screenshot = await page.screenshot(
+                {fullPage: true}
+            );
+
+            // Set file type to png
+            const file = new File([screenshot], newsPage.name + ".png", {type: "image/png"});
+
+            // Set filename with date
+            const date = Date.now()
+            const filename = newsPage.name + "_" + date + ".png"
+
+            console.log("[x] Upload news " + newsPage.name + " archive...")
+            // Upload file to Supabase Storage
+            const {data, error} = await supabase.storage
+                .from("news-archives")
+                .upload(newsPage.name + "/" + filename, file, {
+                    cacheControl: "3600",
+                    upsert: false,
+                });
+            console.log("[x] Data returned from Supabase: ", data)
+            console.log("[x] Error returned from Supabase: ", error)
+            console.log("[x] Done uploading " + newsPage.name + " news archive")
+        }
 
         console.log("[x] Done scraping news")
-        console.log("[x] Publishing news...")
-        console.log("[x] Done publishing news")
-
-        // return new Response(screenshot, {
-        //     headers: {"Content-Type": "image/png"},
-        // });
 
         return new Response(JSON.stringify({message: "News scraped successfully"}), {
             headers: {"Content-Type": "application/json"},
